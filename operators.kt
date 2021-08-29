10b4eefb24464fe09437debe4dd960f65550be09
mustafaalmhboub <89340300+mustafaalmhboub@users.noreply.github.com>
2021-08-29 17:07:41 +0100

    Add files via upload

---
+++ operators.kt
@@ -0,0 +1,23 @@
+
+
+
+   العمليات الحسابية في لغة كوتلن :
+
+    fun main(args: Array<String>){
+
+      var number1 = 5 
+      var number2 = 3 
+      var sum = number1 + number3 
+      var sub = number1 - number3 
+      var mult = number1 * number3 
+      var divide = number1 / number3 
+      var mod = number1 % number3 
+     
+    println("$sum")
+    println("$sub")
+    println("$mult")
+    println("$divide")
+    println("$mod")
+   
+    }
+
